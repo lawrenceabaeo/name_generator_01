76c940096555cd3ce932f2e6a75b9d67c81ab3c6
Lawrence Abaeo <labaeo@gmail.com>
2016-01-18 11:56:42 -0800

    Content tweaks again.

--- includes/description.php
+++ includes/description.php
@@ -1,6 +1,6 @@
 <h3>What is Diamond Dogs?</h3>
 
-<p>Diamond Dogs is a military organization in the video game <a href="https://en.wikipedia.org/wiki/Metal_Gear_Solid_V:_The_Phantom_Pain">'Metal Gear: The Phantom Pain'</a>. From the <a href="http://metalgear.wikia.com/wiki/Diamond_Dogs
+<p>Diamond Dogs is an organization in the video game <a href="https://en.wikipedia.org/wiki/Metal_Gear_Solid_V:_The_Phantom_Pain">'Metal Gear: The Phantom Pain'</a>. From the <a href="http://metalgear.wikia.com/wiki/Diamond_Dogs
   ">Wikia:</a>
   <blockquote cite="http://metalgear.wikia.com/wiki/Diamond_Dogs
 " class="style2">Diamond Dogs was a private military company led by Venom Snake around the 1980s.</blockquote>
@@ -8,4 +8,4 @@
 
 <h3>Why do they get code names?</h3>
 
-<p>Code names provide some concealment for Diamond Dog members; the names also help separate them from their previous lives and connections. Unlike other organizations, where names are earned through experiences or character, Diamond Dog names are generated automatically upon recruitment.</p>
+<p>Code names provide some concealment for Diamond Dog members; the names also separate them from their previous lives and connections. Unlike other organizations, where names are earned through experiences or character, Diamond Dog names are generated automatically upon recruitment.</p>
